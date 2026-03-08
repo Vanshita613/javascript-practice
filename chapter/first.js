05b5f4774f1d4969c60100cb9030efd5e2d6d038
Vanshita613 <kalyanevanshita07@gmail.com>
2026-03-08 18:08:46 +0530

    javascript practice

---
+++ chapter/first.js
@@ -0,0 +1,14 @@
+// question1- create a const object called "product" to store info in the pict//
+const product={
+    title:"ball pen",
+    rating:4,
+    offer: 5,
+    price:270,
+}
+console.log(product);
+
+// Output//
+offer: 5
+price: 270
+rating: 4
+title: "ball pen"
