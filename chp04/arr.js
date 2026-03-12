adc8617ab4dc3b369a0e1606dde148e0dea79962
Vanshita613 <kalyanevanshita07@gmail.com>
2026-03-12 19:24:34 +0530

    ..

--- chp04/arr.js
+++ chp04/arr.js
@@ -1,7 +1,7 @@
 //ARRAYS
 
-let marks = [88, 59, 85, 30];
-console.log(marks);
+let _marks = [88, 59, 85, 30];
+console.log(_marks);
 
 //array loop
 
@@ -20,3 +20,41 @@ for (let idx = 0; idx < names.length; idx++) {
 for (let name of names) {
     console.log(name);
 }
+
+
+
+//QUESTION-1. Qs. For a given array with marks of students -> [85, 97, 44, 37, 76, 60] Find the average marks of the entire class.
+
+let marks = [78, 94, 88, 93, 76, 65];
+
+let sum = 0;
+
+for (let val of marks) {
+    sum = +val;
+
+}
+
+let avg = sum / marks.length;
+console.log(`avg marks of the class = ${avg}`)
+
+
+//QUESTION-2 
+
+
+
+let items = [250, 645, 300, 900, 50];
+let i = 0;
+// for (let val of items) {
+//     let offer = val / 10;
+//     items[i] = items[i] - offer;
+//     console.log(`value after =${items}`);
+//     i++;
+// }
+
+for (let i = 0; i < items.length; i++) {
+    let offer = items[i] / 10;
+    items[i] -= offer;
+
+}
+console.log(items);
+
