16d9004fd4d210a60fc672316dee413358f1e2cf
Vanshita613 <kalyanevanshita07@gmail.com>
2026-03-12 19:08:46 +0530

    new chapter added

---
+++ chp04/arr.js
@@ -0,0 +1,22 @@
+//ARRAYS
+
+let marks = [88, 59, 85, 30];
+console.log(marks);
+
+//array loop
+
+let sub = ["maths", "chemistry", "physics", "english", "hindi"];
+console.log(sub[2]);
+
+
+let names = ["sia", "colby", "sam", "jin", "niki"];
+
+for (let idx = 0; idx < names.length; idx++) {
+    console.log(names[idx]);
+}
+
+//for of
+
+for (let name of names) {
+    console.log(name);
+}
