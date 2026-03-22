9a6a019e94973881c6248285ab4dea59d304f96e
Vanshita613 <kalyanevanshita07@gmail.com>
2026-03-22 12:57:41 +0530

    js

--- chp05/funct.js
+++ chp05/funct.js
@@ -11,13 +11,25 @@
 //     console.log(a+b);
 // }
 
-//return value
-function sum(a, b) {
-    s = a + b;
-    return s;
+// //return value
+// function sum(a, b) {
+//     s = a + b;
+//     return s;
+// }
+// let val = sum(8, 8);
+// console.log(val);
+
+
+//arrow fnct
+const arrowSum = (a, b) => {
+    console.log(a + b);
+};
+
+
+//multiplication funt
+const arrowMul = (x, y) => {
+    console.log(x * y);
 }
-let val = sum(8, 8);
-console.log(val);
 
 
 
